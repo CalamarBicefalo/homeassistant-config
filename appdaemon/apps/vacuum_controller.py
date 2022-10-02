67846afd83d155d2b7c5182c4aceb45d925f2242
Jose Carlos Valero Sanchez <josecarlosvalerosanchez@gmail.com>
2022-10-02 21:38:38 +0100

    rearrange files

--- appdaemon/apps/vacuum_controller.py
+++ appdaemon/apps/vacuum_controller.py
@@ -6,8 +6,10 @@ import appdaemon.plugins.hass.hassapi as hass
 class VacuumController(hass.Hass):
 
     def initialize(self):
-        self.log("Initializing vacuum controller", level="DEBUG")
+        self.log("Initializing vacuum controller now", level="WARNING")
         self.run_daily(self.clean_kitchen, time(hour=22))
 
     def clean_kitchen(self, entity, attribute, old, new, kwargs):
         self.log("Cleaning kitchen")
+        self.call_service("xiaomi_miio.vacuum_clean_segment", entity_id = "roborock.vacuum.a15", targets = 16)
+
