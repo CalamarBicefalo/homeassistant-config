faac86be66a13768c5ce34c0dcda3b04b0e1df8a
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-10-05 19:36:07 +0100

    chore: improve logs

--- appdaemon/apps/vacuum_controller.py
+++ appdaemon/apps/vacuum_controller.py
@@ -17,12 +17,16 @@ class VacuumController(hass.Hass):
         )
 
     def clean_kitchen(self, kwargs):
-        self.log("Cleaning kitchen", level="INFO")
+        self.log("Triggering kitchen clean task", level="INFO")
         last_cooked = datetime.strptime(str(self.get_state(entities.HELPER_LAST_COOKED)), "%Y-%m-%d %H:%M:%S")
         days_since_kitchen_got_dirty = datetime.now() - last_cooked
         if days_since_kitchen_got_dirty.days == 0:
+            self.log('Cleaning kitchen', level="INFO")
             self.call_service(
                 "xiaomi_miio/vacuum_clean_segment",
                 entity_id=entities.DEVICE_VACUUM_CLEANER,
                 segments=16
             )
+        else:
+            self.log(f'Ignoring kitchen clean because we have not cooked for {days_since_kitchen_got_dirty} days.',
+                     level="INFO")
