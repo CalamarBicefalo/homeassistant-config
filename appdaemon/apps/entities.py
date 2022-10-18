d6dc7290bb8bdbf59bad6aa2a046a2eb2414564d
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-10-18 18:54:25 +0100

    feat: wait to clean kitchen until TV is off

--- appdaemon/apps/entities.py
+++ appdaemon/apps/entities.py
@@ -1,2 +1,3 @@
 HELPER_LAST_COOKED = "input_datetime.last_cooked"
 DEVICE_VACUUM_CLEANER = "vacuum.roborock_vacuum_a15"
+TV = "media_player.sony_kd_49xf8096"
--- appdaemon/apps/vacuum_controller.py
+++ appdaemon/apps/vacuum_controller.py
@@ -2,12 +2,14 @@ from datetime import datetime
 
 import appdaemon.plugins.hass.hassapi as hass
 
-from apps import entities
+import entities
+import services
 
 
 class VacuumController(hass.Hass):
 
     def initialize(self):
+        self.log(f'tv state {self.get_state(entities.TV)}')
         time = "22:30:00"
         self.log(f'Initializing vacuum controller at {time}.', level="INFO")
         self.run_daily(
@@ -16,14 +18,17 @@ class VacuumController(hass.Hass):
 
         )
 
-    def clean_kitchen(self, kwargs):
+    async def clean_kitchen(self, kwargs):
         self.log("Triggering kitchen clean task", level="INFO")
         last_cooked = datetime.strptime(str(self.get_state(entities.HELPER_LAST_COOKED)), "%Y-%m-%d %H:%M:%S")
         days_since_kitchen_got_dirty = datetime.now() - last_cooked
         if days_since_kitchen_got_dirty.days == 0:
+            while self.get_state(entities.TV) == "on":
+                self.log('TV is on, waiting until it is off', level="INFO")
+                await self.sleep(600)
             self.log('Cleaning kitchen', level="INFO")
             self.call_service(
-                "xiaomi_miio/vacuum_clean_segment",
+                services.VACUUM_CLEAN_SEGMENT,
                 entity_id=entities.DEVICE_VACUUM_CLEANER,
                 segments=16
             )
