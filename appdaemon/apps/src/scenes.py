0852b58f132e538f1656337edfed44fa07674982
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-10-29 19:58:17 +0100

    fix: scene name

--- appdaemon/apps/src/scenes.py
+++ appdaemon/apps/src/scenes.py
@@ -1,3 +1,3 @@
 LIVING_ROOM_READING = 'scene.living_room_reading'
 LIVING_ROOM_MOVIE = 'scene.living_room_movie'
-LIVING_ROOM_WELCOME = 'scene.full_living_room_welcome'
+LIVING_ROOM_WELCOME = 'scene.living_room_welcome'
