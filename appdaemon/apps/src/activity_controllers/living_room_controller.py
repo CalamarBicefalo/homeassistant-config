c86d3cddd021f1ddf4097d93c5ab19816d64b3d5
Jose Carlos Valero Sanchez <josecarlosvalerosanchez@gmail.com>
2022-11-09 18:14:33 +0000

    fix: add sofa sensor for living room scenes

--- appdaemon/apps/src/activity_controllers/living_room_controller.py
+++ appdaemon/apps/src/activity_controllers/living_room_controller.py
@@ -10,7 +10,7 @@ from app import App
 class LivingRoomActivity(ControllerApp):
     motion_sensor = entities.BINARY_SENSOR_LIVING_ROOM_MOTION
     activity_helper = helpers.LIVING_ROOM_ACTIVITY
-    additional_triggers = [entities.MEDIA_PLAYER_TV]
+    additional_triggers = [entities.MEDIA_PLAYER_TV, entities.BINARY_SENSOR_SOFA_PS_WATER]
 
     async def get_custom_activity(self, entity, attribute, old, new) -> Optional[activities.Activity]:
         if await self.is_on(entities.MEDIA_PLAYER_TV):
