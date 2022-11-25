f9bb352d29d1ff5adc37bca6b293ac7f164c52ac
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-11-25 14:19:59 +0000

    feat: add storage room activity

--- appdaemon/apps/src/activity_controllers/storage_room_controller.py
+++ appdaemon/apps/src/activity_controllers/storage_room_controller.py
@@ -1,2 +1,20 @@
-class StorageRoomController:
-    pass
+import activities
+import entities
+from app import App
+
+
+class StorageRoomController(App):
+    def initialize(self) -> None:
+        self.log(f'Initializing storage room controller.', level="DEBUG")
+
+        self.listen_state(
+            self.controller_handler,
+            [entities.BINARY_SENSOR_STORAGE_ROOM_CS_CONTACT]
+        )
+
+    def controller_handler(self, entity, attribute, old, new, kwargs):  # type: ignore
+        if self.is_on(entities.BINARY_SENSOR_STORAGE_ROOM_CS_CONTACT) :
+            self.activities.storageroom.set(activities.StorageRoom.PRESENT)
+        else:
+            self.activities.storageroom.set(activities.StorageRoom.EMPTY)
+
