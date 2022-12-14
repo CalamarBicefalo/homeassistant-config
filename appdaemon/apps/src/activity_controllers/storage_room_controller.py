d03c57662ef0efdcea7ee011c321d5d1cf366d40
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-12-14 22:30:45 +0000

    feat: turn off storage room lights after 10 minutes

--- appdaemon/apps/src/activity_controllers/storage_room_controller.py
+++ appdaemon/apps/src/activity_controllers/storage_room_controller.py
@@ -4,6 +4,8 @@ from app import App
 
 
 class StorageRoomController(App):
+    set_to_away_in_10_minutes = None
+
     def initialize(self) -> None:
         self.log(f'Initializing storage room controller.', level="DEBUG")
 
@@ -13,7 +15,12 @@ class StorageRoomController(App):
         )
 
     def controller_handler(self, entity, attribute, old, new, kwargs):  # type: ignore
+        if self.set_to_away_in_10_minutes:
+            self.cancel_timer(self.set_to_away_in_10_minutes)
+            self.set_to_away_in_10_minutes = None
+
         if self.is_on(entities.BINARY_SENSOR_STORAGE_ROOM_CS_CONTACT):
             self.activities.storageroom.set(activities.StorageRoom.PRESENT)
+            self.set_to_away_in_10_minutes = self.run_in(lambda: self.activities.storageroom.set(activities.StorageRoom.EMPTY), 6000)
         else:
             self.activities.storageroom.set(activities.StorageRoom.EMPTY)
