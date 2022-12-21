02c1ad21af18ccb14a593996d741f5fb643b5802
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-12-21 08:50:51 +0000

    fix: adjust day/night deltas

--- appdaemon/apps/src/mode_manager.py
+++ appdaemon/apps/src/mode_manager.py
@@ -9,23 +9,20 @@ class ModeManager(App):
 
     def initialize(self) -> None:
         self.log(f'Initializing mode manager.', level="DEBUG")
-        self.run_at_sunrise(self.on_day, offset=timedelta(minutes=30).total_seconds())
-        self.run_at_sunset(self.on_night, offset=timedelta(minutes=-30).total_seconds())
+        self.run_at_sunrise(self.on_schedule)
+        self.run_at_sunset(self.on_schedule)
         self.listen_event(self.on_alarm_dismissed, "SleepAsAndroid_phone")
 
-    def on_day(self, kwargs: Any) -> None:
-        self.on_schedule(Mode.DAY)  # type: ignore
-
-    def on_night(self, kwargs: Any) -> None:
-        self.on_schedule(Mode.NIGHT)  # type: ignore
-
-    def on_schedule(self, mode: Mode) -> None:
+    def on_schedule(self, kwargs: Any) -> None:
         if self.mode.get() not in [Mode.AWAY, Mode.BEDTIME, Mode.SLEEPING]:
-            self.mode.set(mode)
+            self.set_mode_by_time()
 
     def on_alarm_dismissed(self, event_name: str, data: Any, kwargs: Any) -> None:
         if data['event'] == 'alarm_alert_dismiss':
-            if self.sunset() < self.sunrise():
-                self.mode.set(Mode.DAY)
-            else:
-                self.mode.set(Mode.NIGHT)
+            self.set_mode_by_time()
+
+    def set_mode_by_time(self) -> None:
+        if self.sunset() < self.sunrise():
+            self.mode.set(Mode.DAY)
+        else:
+            self.mode.set(Mode.NIGHT)
