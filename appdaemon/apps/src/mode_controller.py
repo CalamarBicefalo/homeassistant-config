694556f456190d270ae23155e21fa549be63b6ec
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-12-23 17:03:22 +0000

    feat: make sure music is OFF for appropriate modes

--- appdaemon/apps/src/mode_controller.py
+++ appdaemon/apps/src/mode_controller.py
@@ -24,8 +24,7 @@ class ModeController(App):
     def controller_handler(self, entity: Any, attribute: Any, old: Any, new: Any, kwargs: Any) -> None:
         match new:
             case Mode.BEDTIME:
-                self.turn_off(entities.MEDIA_PLAYER_TV)
-                self.turn_off(entities.MEDIA_PLAYER_SONY_KD_49XF8096)
+                self.call_service(services.MEDIA_PLAYER_TURN_OFF, entity_id="all")
                 self.turn_on(entities.SCENE_HOME_CORRIDOR)
                 self.turn_on(entities.SCENE_BEDROOM_BRIGHT)
                 self.turn_on(entities.SWITCH_PREPARE_ME_TO_GO_TO_SLEEP_HUE_LABS_FORMULA)
@@ -46,6 +45,7 @@ class ModeController(App):
                 self.call_service("cover/open_cover",
                                   entity_id=entities.COVER_BLINDS)
             case Mode.SLEEPING:
+                self.call_service(services.MEDIA_PLAYER_TURN_OFF, entity_id="all")
                 self.turn_off(entities.MEDIA_PLAYER_TV)
                 self.turn_off(entities.SWITCH_DRUMS_PLUG)
                 self.turn_off(entities.SWITCH_MONITOR_PLUG)
@@ -53,6 +53,7 @@ class ModeController(App):
                 self.bedroom_music.play(Tune.RAIN, volume_level=0.2)
 
             case Mode.AWAY:
+                self.call_service(services.MEDIA_PLAYER_TURN_OFF, entity_id="all")
                 self.call_service(services.LIGHT_TURN_OFF, entity_id="all")
                 self.turn_off(entities.MEDIA_PLAYER_HOME)
                 self.turn_off(entities.MEDIA_PLAYER_TV)
