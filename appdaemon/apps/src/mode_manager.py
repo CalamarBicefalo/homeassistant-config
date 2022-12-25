bddf45c770985528786fc19cfa49c2161a17111f
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-12-25 18:03:18 +0000

    feat: add notifications for mode manager

--- appdaemon/apps/src/mode_manager.py
+++ appdaemon/apps/src/mode_manager.py
@@ -31,3 +31,6 @@ class ModeManager(App):
             self.mode.set(Mode.DAY)
         else:
             self.mode.set(Mode.NIGHT)
+        self.call_service("notify/notify",
+                          message=f'Mode set to {self.mode.get()}',
+                          title=f'Mode set to {self.mode.get()}')
