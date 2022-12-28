f02e6631c7acb88b52d08d913a0ec422bbaeefc5
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-12-28 09:00:26 +0000

    chore: add tests for media in living room

--- appdaemon/tests/scene_controllers/living_room_scene_test.py
+++ appdaemon/tests/scene_controllers/living_room_scene_test.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 from appdaemontestframework import automation_fixture, given_that as given
 
@@ -8,6 +10,7 @@ import matchers
 import modes
 import scenes
 import states
+from music import MusicHandler
 from scene_controllers.living_room_scene import LivingRoomScene
 
 
@@ -26,12 +29,26 @@ def test_reading_sets_reading_scene(given_that, living_room_scene, assert_that):
     assert_that(scenes.LIVING_ROOM_READING.get()).was.turned_on()
 
 @pytest.mark.asyncio
-def test_reading_plays_music(given_that, living_room_scene, assert_that):
-    given_that.living_room_scene_is(activity=activities.LivingRoom.READING, illuminance=30, are_lights_on=False)
+def test_reading_plays_music(given_that) -> None:
+    given_that.living_room_scene_is(activity=activities.LivingRoom.PRESENT)
 
-    living_room_scene.handle_scene(None, None, None, None, None)
+    with mock.patch.object(MusicHandler, 'play') as music:
+        scene = LivingRoomScene(None, LivingRoomScene.__class__, None, None, None, None, None)
+        scene.music = music
+        scene.on_activity_change(activities.LivingRoom.READING)
 
-    assert_that(scenes.LIVING_ROOM_READING.get()).was.turned_on()
+        music.play.assert_called_once()
+
+@pytest.mark.asyncio
+def test_reading_does_not_replace_if_playing(given_that) -> None:
+    given_that.living_room_scene_is(activity=activities.LivingRoom.PRESENT, playing_music=states.ON)
+
+    with mock.patch.object(MusicHandler, 'play') as music:
+        scene = LivingRoomScene(None, LivingRoomScene.__class__, None, None, None, None, None)
+        scene.music = music
+        scene.on_activity_change(activities.LivingRoom.READING)
+
+        music.play.assert_not_called()
 
 
 @pytest.mark.asyncio
@@ -43,8 +60,8 @@ def test_watching_tv_sets_movie_scene(given_that, living_room_scene, assert_that
     assert_that(scenes.LIVING_ROOM_MOVIE.get()).was.turned_on()
 
 
-def living_room_scene_is(self, activity, illuminance=0, are_lights_on=False, mode=modes.Mode.NIGHT):
-    self.state_of(entities.MEDIA_PLAYER_MASS_COOKING_AREA).is_set_to(states.OFF)
+def living_room_scene_is(self, activity, illuminance=0, are_lights_on=False, mode=modes.Mode.NIGHT, playing_music=states.OFF):
+    self.state_of(entities.MEDIA_PLAYER_MASS_COOKING_AREA).is_set_to(playing_music)
     self.state_of(helpers.HOMEASSISTANT_MODE).is_set_to(mode)
     self.state_of(entities.SENSOR_DESK_MS_ILLUMINANCE).is_set_to(illuminance)
     self.state_of(activities.livingroom_helper).is_set_to(activity)
