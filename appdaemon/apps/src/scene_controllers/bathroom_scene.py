62a4066ebfb98ea68c8f46bbd4eeb0023410baf7
José Carlos Valero Sánchez <jose.valero@snyk.io>
2024-11-23 22:08:53 +0000

    feat: add bathroom scene

---
+++ appdaemon/apps/src/scene_controllers/bathroom_scene.py
@@ -0,0 +1,25 @@
+import entities
+import scenes
+from rooms import *
+from scene_controllers import scene
+from scene_controllers.scene import SceneByModeSelector, Scene
+from scene_controllers.scene_app import SceneApp
+from select_handler import SelectHandler
+from selects import Mode
+
+
+class BathroomScene(SceneApp):
+    @property
+    def activity(self) -> SelectHandler:
+        return self.handlers.rooms.ensuite.activity
+    illuminance_sensor = None
+    room_lights = entities.LIGHT_BATHROOM
+
+    def get_light_scene(self, activity: StrEnum) -> Scene | SceneByModeSelector:
+        if activity in [Bathroom.Activity.PRESENT, Bathroom.Activity.SHOWERING]:
+            return scene.by_mode({
+                Mode.DAY: scenes.BATHROOM_CONCENTRATE,
+                Mode.NIGHT: scenes.BATHROOM_CONCENTRATE,
+                Mode.SLEEPING: scenes.BATHROOM_NIGHTLIGHT,
+            })
+        return scene.off()
