854534d1465d5b6efd7e5c12bb275f48983245c4
José Carlos Valero Sánchez <jose.valero@snyk.io>
2025-12-29 19:03:02 +0000

    fix: office blinds

--- appdaemon/apps/src/scene_controllers/office_scene.py
+++ appdaemon/apps/src/scene_controllers/office_scene.py
@@ -19,8 +19,8 @@ class OfficeScene(SceneApp):
     illuminance_sensor = entities.SENSOR_MS_STUDIO_EP1_ILLUMINANCE
     room_lights = entities.LIGHT_OFFICE
     speakers = entities.MEDIA_PLAYER_OFFICE_SPEAKER_2
-    # blinds = entities.COVER_OFFICE_BLINDS
-    # room_has_plants = False
+    blinds = entities.COVER_OFFICE_BLINDS
+    room_has_plants = True
 
     def get_light_scene(self, activity: Office.Activity, previous_activity: Optional[StrEnum]) -> Scene | SceneByModeSelector:
         match activity:
@@ -28,21 +28,21 @@ class OfficeScene(SceneApp):
                 return scene.with_actions(
                     scenes.OFFICE_NATURAL_LIGHT_3,
                     lambda: self.play_music_if_appropriate(),
-                    # lambda: self.handlers.blinds.best_for_temperature(),
+                    lambda: self.handlers.blinds.best_for_temperature(),
                 )
 
             case Office.Activity.MEETING:
                 return scene.with_actions(
                     scenes.OFFICE_NATURAL_LIGHT_3,
                     lambda: self.handlers.music.pause(),
-                    # lambda: self.handlers.blinds.best_for_temperature(),
+                    lambda: self.handlers.blinds.best_for_temperature(),
                 )
 
             case Office.Activity.PRESENT:
                 return scene.with_actions(
                     scenes.OFFICE_NATURAL_LIGHT_3,
                     lambda: self.pause_music_if_working_before(previous_activity),
-                    # lambda: self.handlers.blinds.best_for_temperature(),
+                    lambda: self.handlers.blinds.best_for_temperature(),
                 )
 
         return scene.with_actions(
@@ -51,9 +51,9 @@ class OfficeScene(SceneApp):
             # lambda: self.handlers.blinds.best_for_temperature(),
         )
 
-    # def on_mode_change(self, new: Mode, old: Mode) -> None:
-    #     if new == Mode.AWAY:
-    #         self.handlers.blinds.best_for_temperature()
+    def on_mode_change(self, new: Mode, old: Mode) -> None:
+        if new == Mode.AWAY:
+            self.handlers.blinds.best_for_temperature()
 
     def play_music_if_appropriate(self) -> None:
         if not self.handlers.music.is_playing():
